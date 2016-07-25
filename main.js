bd1e2da9460f2f4f4055ef0ba2c7938831cc1d30
NicoleAgurcia <nicole_agurcia@unitec.edu>
2016-07-25 13:05:51 -0600

    Desencriptacion de mensaje-codigo de cesar

--- main.js
+++ main.js
@@ -5,22 +5,33 @@ const SeparateString =  (string) => {
   return ArrString;
 }
 
-
 const ConvertToChar = (arr) =>{return ConvertToChar = String.fromCharCode(arr);}
-          
+
+const message = (Text,move) => {
+  console.log("----Código de César----");
+  console.log("Original: '" + Text + "'");
+  console.log("Desplazamiento: '" + move + "'");
+}          
 
 const encrypting = (Text,move) =>{
+  message(Text,move);
   const ConvertToAscii = (Text) => {return ConvertToAscii = Text.charCodeAt()+move;}
   const ArrAscii = SeparateString(Text).map(ConvertToAscii); 
   const ArrChar = ArrAscii.map(ConvertToChar).join(""); 
-  console.log(ArrChar);
+  console.log("Resultado: '"+ArrChar+"'");
 }
  
    
-encrypting ("hola",2);
+encrypting ("cuaderno",3);
 
- 
-
+const desEncrypting = (Text,move) =>{
+  message(Text,move);
+  const ConvertToAscii = (Text) => {return ConvertToAscii = Text.charCodeAt()-move;}
+  const ArrAscii = SeparateString(Text).map(ConvertToAscii); 
+  const ArrChar = ArrAscii.map(ConvertToChar).join(""); 
+  console.log("Resultado: '"+ArrChar+"'");
+}   
+desEncrypting ("fxdghuqr",3);
 
 
 
